afd802eef61a0802d5d727dc69651bf13667429d
rgzera666 <ranggass77l@gmail.com>
2021-10-08 15:02:36 +0700

    testing deploy heroku

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ubah isi <code>src/App.js</code> jika mau di edit dan jangan lupa di save lalu reload web pages.
+          Ubah isi <code>src/App.js</code> <br>
+          jika mau di edit dan jangan lupa di save lalu reload web pages.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
