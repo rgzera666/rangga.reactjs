f26cff27d83acd35f2965ae77fae8f7ebf1b982d
rgzera666 <ranggass77l@gmail.com>
2021-11-23 15:05:17 +0700

    Testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import Welcome from "./Components/FunctionalComp";
-import FunctionalComp from "./Components/FunctionalComp";
 import Komponen from "./Home/index";
 
 function App() {
   return (
     <div className="App">
-      <Komponen />
-      <Welcome />
+      <Komponen /> 
     </div>
   );
 }
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ClassComponent from "../Components/ClassComponent";
+import Welcome from "../Components/Welcome";
+
 
 export default class Komponen extends React.Component {
 
@@ -7,6 +9,7 @@ export default class Komponen extends React.Component {
         return (
             <div>
                 <ClassComponent />
+                <Welcome />
             </div>
         )
     }
