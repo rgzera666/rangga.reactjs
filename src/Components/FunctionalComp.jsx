318156f14d88474d2fb742f8f148780892b147a9
rgzera666 <ranggass77l@gmail.com>
2021-11-23 14:54:44 +0700

    Local to server

--- src/Components/FunctionalComp.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Welcome() {
-    return <h1>Komponen ini dibuat dengan functional comp</h1>;
-  }
-  
-  export default Welcome;
---
+++ src/Components/Welcome.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Welcome = () => <h1>Membuat komponen dengan functional comp</h1>
+  export default Welcome;
